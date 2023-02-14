2ff9d57614a091a191951aa6657ed6e8d069ae9c
ShabanovVA <1514768zp@gmail.com>
2023-02-14 15:07:03 +0200

    Corrected

--- src/index.js
+++ src/index.js
@@ -8,18 +8,22 @@ const inputEl = document.getElementById('search-box');
 
 const DEBOUNCE_DELAY = 300;
 
-inputEl.addEventListener('input', debounce(onSearchCountry,DEBOUNCE_DELAY));
+inputEl.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
 
 
 function onSearchCountry(e) {
     let inputValue = e.target.value.trim();
 
-    fetchCountries(inputValue)
-    .then(dataCountries => {
-        createMarkupCountryList(dataCountries)
-    })
-    .catch(error => {
-        Notify.failure("Oops, there is no country with that name")
-    })
-    
+    if (e.target.value.trim() !==0) {
+        fetchCountries(inputValue)
+            .then(dataCountries => {
+                createMarkupCountryList(dataCountries)
+            })
+            .catch(error => {
+                Notify.failure("Oops, there is no country with that name")
+            })
+    } else {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+    }
 }
--- src/markupCountry.js
+++ src/markupCountry.js
@@ -5,12 +5,14 @@ const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 countryListEl.style.listStyleType = "none";
 countryListEl.style.paddingLeft = "0";
-    
 const countriesArr = [];
+    
+countryListEl.innerHTML = '';
+countryInfoEl.innerHTML = '';
 
-    if (dataCountries.length > 10) {
+if (dataCountries.length > 10) {
         Notify.info("Too many matches found. Please enter a more specific name.")
-    } else if (dataCountries.length >= 2 && dataCountries.length <= 10) {
+    } else if (dataCountries.length > 1 && dataCountries.length <= 10) {
         countriesArr.push(
         dataCountries.map(item => {
             return `<li class = "country-item">
@@ -22,7 +24,8 @@ const countriesArr = [];
         );
         countryListEl.innerHTML = countriesArr;
         countryInfoEl.innerHTML = '';
-    } else {
+    } 
+    else {
         countriesArr.push(
         dataCountries.map(item => {
             return `<div class="country-name"> 
@@ -33,7 +36,7 @@ const countriesArr = [];
                     <span class="title">${item.name.official}</span></div>
                     <p class="country-info"><b>Capital:</b> ${item.capital}</p>
                     <p class="country-info"><b>Population:</b> ${item.population}</p>
-                    <p class="country-info"><b>languages:</b> ${Object.values(item.languages)}</p>`
+                    <p class="country-info"><b>languages:</b> ${Object.values(item.languages).join(', ')}</p>`
         }).join('')
         )
         countryInfoEl.innerHTML = countriesArr;
