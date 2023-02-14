d6cdfed132b62d38510f20b97e39124301a97ad0
ShabanovVA <1514768zp@gmail.com>
2023-02-14 11:43:18 +0200

    Дз 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
+import createMarkupCountryList from './markupCountry';
 import './css/styles.css';
 
+const inputEl = document.getElementById('search-box');
+
 const DEBOUNCE_DELAY = 300;
+
+inputEl.addEventListener('input', debounce(onSearchCountry,DEBOUNCE_DELAY));
+
+
+function onSearchCountry(e) {
+    let inputValue = e.target.value.trim();
+
+    fetchCountries(inputValue)
+    .then(dataCountries => {
+        createMarkupCountryList(dataCountries)
+    })
+    .catch(error => {
+        Notify.failure("Oops, there is no country with that name")
+    })
+    
+}
---
+++ src/markupCountry.js
@@ -0,0 +1,42 @@
+import { Notify } from "notiflix";
+
+export default function createMarkupCountryList(dataCountries) {
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+countryListEl.style.listStyleType = "none";
+countryListEl.style.paddingLeft = "0";
+    
+const countriesArr = [];
+
+    if (dataCountries.length > 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.")
+    } else if (dataCountries.length >= 2 && dataCountries.length <= 10) {
+        countriesArr.push(
+        dataCountries.map(item => {
+            return `<li class = "country-item">
+                    <img class="country-flags"
+                    src=${item.flags.svg}
+                    alt=${item.name.official} width="30px" height="20px"/>
+                    <span>${item.name.official}</span></li>`
+        }).join('')
+        );
+        countryListEl.innerHTML = countriesArr;
+        countryInfoEl.innerHTML = '';
+    } else {
+        countriesArr.push(
+        dataCountries.map(item => {
+            return `<div class="country-name"> 
+                    <img class="country-flags"
+                    src=${item.flags.svg}
+                    alt=${item.name.official}
+                    width="30px" height="20px"/>
+                    <span class="title">${item.name.official}</span></div>
+                    <p class="country-info"><b>Capital:</b> ${item.capital}</p>
+                    <p class="country-info"><b>Population:</b> ${item.population}</p>
+                    <p class="country-info"><b>languages:</b> ${Object.values(item.languages)}</p>`
+        }).join('')
+        )
+        countryInfoEl.innerHTML = countriesArr;
+        countryListEl.innerHTML = '';
+    };
+}
